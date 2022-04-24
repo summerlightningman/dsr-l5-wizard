2643e1ad416da16d1b482e723c98e30e89bc88af
summerlightningman <div8@bk.ru>
2022-04-24 23:47:38 +0300

    refactoring in Label

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,12 +1,17 @@
 import styled from "styled-components";
+import {LabelProps} from "./types";
 
-const Label = styled.label`
+
+const Label = styled.label<LabelProps>`
   font-family: 'Inter', sans-serif;
   font-style: normal;
   font-weight: 400;
   font-size: 40px;
   line-height: 48px;
   color: #778898;
+  
+  display: flex;
+  align-items: ${props => props.alignItems || 'flex-end'};
 `;
 
 export default Label
