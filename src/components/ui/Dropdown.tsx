09a2574708fe21b666512dbcbf511c907097bbe6
summerlightningman <div8@bk.ru>
2022-04-24 20:28:13 +0300

    Added styled component Dropdown

---
+++ src/components/ui/Dropdown.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const Dropdown = styled.select`
+  background: #778898;
+  padding: 11px;
+
+  border: none;
+
+  color: #FFF;
+  font-family: 'Inter', sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 40px;
+  line-height: 48px;
+
+  outline: none;
+`;
+
+export default Dropdown
