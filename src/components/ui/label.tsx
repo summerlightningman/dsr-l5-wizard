4985c9e5f13be4cfe13a5d3e0d88739a9e2fc2fb
summerlightningman <div8@bk.ru>
2022-04-24 18:47:49 +0300

    added Label component

---
+++ src/components/ui/label.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Label = styled.label`
+  font-family: 'Inter', sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 40px;
+  line-height: 48px;
+  color: #778898;
+`;
+
+export default Label
