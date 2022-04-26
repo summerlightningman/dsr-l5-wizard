ae08fcfd92459399bd6af55c82b16f43a2601fde
summerlightningman <div8@bk.ru>
2022-04-26 14:23:06 +0300

    added styled-component Text

---
+++ src/components/ui/text.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Text = styled.span`
+  font-family: 'Inter', sans-serif;
+  font-style: normal;
+  font-weight: 800;
+  font-size: 40px;
+  line-height: 48px;
+
+  color: #778898;
+`;
+
+export default Text
